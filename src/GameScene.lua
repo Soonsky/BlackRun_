eb12ebde5dc536f5163e3759af9d25592f2cb9b2
Soonsky <247383020@qq.com>
2015-07-20 16:26:12 +0800

    准备重构

--- src/GameScene.lua
+++ src/GameScene.lua
@@ -22,22 +22,34 @@ function GameScene:ctor()
     self.draw = nil             --初始化绘制节点
     self.ScheldID = nil         --定时器ID
     self.isTouch = false        --可视为定时器开关  当触摸时 开启 否则关闭
-    self.LineLong = 400           -- 桥的起始长度
+    self.LineLong = 0          -- 桥的起始长度
     self.LineSpeed = 5              --桥的增长速度
-    self.StartPoint = cc.p(50,400)
+    self.isRotating = false         --旋转动画控制器
+    self.StartPoint = cc.p(0,0)
     self:init()
+    
+    --text
+    self.layerout = nil
 end
 
 --初始化界面
 function GameScene:init()
-    self.draw = cc.DrawNode:create()
-    self:addChild(self.draw)
+    
+    
+    self.layerout = ccui.Layout:create()
+    self.layerout:setBackGroundColor(cc.c3b(1,1,1))
+    self.layerout:setBackGroundColorType(ccui.LayoutBackGroundColorType.solid)
+    self.layerout:setContentSize(cc.size(500,900))
+    self.layerout:setPosition(cc.p(50,400))
+    self:addChild(self.layerout)
 
+    self.draw = cc.DrawNode:create()
+    self.layerout:addChild(self.draw)
 
     --绑定触摸时间
     local function onTouchBegan(touch, event)
-        self:LongChange();
         self.isTouch = true;
+        self:LongChange();
         print("dfgfdg"..touch:getLocation().y)
         return true
     end
@@ -49,7 +61,8 @@ function GameScene:init()
             self.ScheldID = nil
         end
         self.isTouch = false     
-        self.LineLong = 400
+        self.LineLong = 0
+        self:LineRotation()
     end
 
     local listener = cc.EventListenerTouchOneByOne:create()
@@ -61,6 +74,12 @@ function GameScene:init()
     eventDispatcher:addEventListenerWithSceneGraphPriority(listener, self)
 end
 
+function GameScene:LineRotation()
+--    self.isRotating = true
+    self.draw:runAction(cc.Spawn:create(cc.RotateTo:create(1,90),cc.ScaleBy:create(1,0.5)))
+--self.draw:setRotation(90)
+end
+
 --桥长度更新函数
 function GameScene:LongChange()
     if self.ScheldID ~= nil then 
@@ -68,12 +87,12 @@ function GameScene:LongChange()
         self.ScheldID = nil
     end
     local function updataLine()
-        if self.isTouch == true then
+        if self.isTouch == true and self.isRotating == false then
             if self.draw ~= nil then
                 self.draw:clear()
             end
             self.LineLong = self.LineLong + self.LineSpeed
-            self.draw:drawSegment(self.StartPoint,cc.p(50,self.LineLong),2,cc.c4b(1,1,1,1.0))
+            self.draw:drawSegment(self.StartPoint,cc.p(0,self.LineLong),2,cc.c4b(1,1,1,1.0))
             print(self.LineLong)
         end
     end
